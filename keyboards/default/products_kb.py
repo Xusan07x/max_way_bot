ed16a9620d97d54e4d511f4a92c36fda315a8607
Xusan07x <suhbatullaev.05@gmail.com>
2024-09-25 15:53:59 +0300

    birinchi commitimiz

---
+++ keyboards/default/products_kb.py
@@ -0,0 +1,31 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from loader import db
+
+
+def make_product_keyboard(category_name: str):
+    products = db.get_products_by_category_name(category_name)
+    buttons = []
+
+    menu = ReplyKeyboardMarkup(keyboard=[], resize_keyboard=True, row_width=2)
+    
+    for product in products:
+        buttons.append(
+            KeyboardButton(text=product[0])
+        )
+
+    menu.keyboard.append(
+        buttons
+    )
+
+    return menu
+
+def make_cart_sub_keyboard():
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [
+                KeyboardButton(text='◀️ orqaga'),
+                KeyboardButton(text="🛒 savat")
+            ]
+        ],
+        resize_keyboard=True
+    )
